3111e274fa25f3813208ebe161efc0784c80044b
marcotuliopin <marcotuliopin@gmail.com>
2023-06-04 21:22:22 -0300

    first commit

--- testes/modelo.test.js
+++ testes/modelo.test.js
@@ -23,3 +23,34 @@ test('Testando cadastro de três perguntas', () => {
   expect(perguntas[2].num_respostas).toBe(0);
   expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta-1);
 });
+
+test('Testando cadastro de seis respostas', () => {
+  modelo.cadastrar_resposta(0, '1');
+  modelo.cadastrar_resposta(0, '2');
+  modelo.cadastrar_resposta(1, '4');
+  modelo.cadastrar_resposta(1, '3');
+  modelo.cadastrar_resposta(1, '4');
+  modelo.cadastrar_resposta(2, '4');
+  const respostas = Array.from({ length: 3 }, (_, i) => modelo.get_respostas(i));
+  expect(respostas[0].length).toBe(2);
+  expect(respostas[1].length).toBe(3);
+  expect(respostas[2].length).toBe(1);
+  expect(respostas[0][0].texto).toBe('1');
+  expect(respostas[0][1].texto).toBe('2');
+  expect(respostas[2][0].texto).toBe('4');
+});
+
+test('Testando get', () => {
+  expect(modelo.get_pergunta(0)).toBeUndefined();
+  expect(modelo.get_respostas(0).length).toBe(0);
+  expect(modelo.get_num_respostas(0)).toBe(0);
+  modelo.cadastrar_pergunta('2 + 2 = ?');
+  const pergunta = modelo.listar_perguntas(); 
+  expect({
+      ...modelo.get_pergunta(pergunta[0].id_pergunta),
+      num_respostas: 0
+  }).toEqual(pergunta[0])
+  modelo.cadastrar_resposta(0, '4');
+  expect(modelo.get_num_respostas(0)).toBe(1)
+  expect(modelo.get_respostas(0)[0].texto).toBe('4')
+});
